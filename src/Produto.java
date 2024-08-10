072f8f45ef578f9e498ac51e05a7c364000ee41d
Luiz Gustavo da Silva <38591825+Luiz4us8av6@users.noreply.github.com>
2024-08-10 15:26:55 -0300

    [Luiz] feat: implementação da classe produto

---
+++ src/Produto.java
@@ -0,0 +1,42 @@
+public class Produto {
+    private int codigo;
+    private String nome;
+    private int quantidade;
+
+    public Produto(int codigo, String nome, int quantidade) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.quantidade = quantidade;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    // Para exibir o produto de forma bonita
+    @Override
+    public String toString() {
+        return "Produto: Código= " + this.codigo + ", Nome= " + this.nome + ", Quantidade= " + this.quantidade;
+    }
+}
